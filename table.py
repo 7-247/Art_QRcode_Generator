9ac5cdcaf6f9e34bfe59d7d0252bf3df84029b55
caoyandidi1103 <didiking@yeah.net>
2022-02-22 15:05:25 +0800

    add database

--- table.py
+++ table.py
@@ -1,9 +1,10 @@
 import sqlite3
-con = sqlite3.connect("Class.db",check_same_thread=False)
+
+con = sqlite3.connect("Class.db", check_same_thread=False)
 cursor = con.cursor()
 cursor.execute("select * from USED")
 result = cursor.fetchall()
 for i in result:
-	print(i)
+    print(i)
 cursor.close()
 con.close()
